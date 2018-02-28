4148c1847eef1fb460875266deebc551b6fed087
igornerest <igornerest@gmail.com>
2018-02-28 12:09:06 -0300

    alguns erros a serem corrigidos

--- lista_ligada/ll-simples.c
+++ lista_ligada/ll-simples.c
@@ -9,7 +9,7 @@ struct LinkedNode {
   LinkedNode *next;
 };
 
-// adiciona um elemento a lista
+// adiciona um elemento a lista e retorna a posicao desse tempo;
 LinkedNode *appendNode(LinkedNode *first, int num){
   LinkedNode *tmp = malloc(sizeof(LinkedNode));
   if(tmp == NULL) return NULL;
@@ -25,7 +25,18 @@ LinkedNode *appendNode(LinkedNode *first, int num){
   return tmp;
 }
 
-// concatena o primeiro argumento ao segundo
+LinkedNode *addNode(LinkedNode *seq, int position){
+  int i, num;
+  for(i = 0; i < position && seq != NULL; i++)
+    seq = seq->next;
+  if (seq == NULL) return NULL;
+
+  printf("Qual o valor do proximo elemento da lista? ");
+  scanf("%d", &num);
+  LinkedNode *tmp = appendNode(seq - i, num);
+}
+
+// concatena a primeira lista (como argumento) a segunda
 void *connectList (LinkedNode *f1, LinkedNode *f2){
   LinkedNode *tmp = f1;
   while(tmp->next != NULL)
@@ -36,7 +47,7 @@ void *connectList (LinkedNode *f1, LinkedNode *f2){
 // imprime a lista ligada
 void printList(LinkedNode *first){
   LinkedNode *tmp = first;
-  printf("\nSequencia: ");
+  printf("Sequencia: ");
   while(tmp != NULL){
     printf("%d ", tmp->data);
     tmp = tmp->next;
@@ -49,34 +60,48 @@ LinkedNode *makeList (){
   LinkedNode *tmp = NULL, *first = NULL;
   int i, num;
 
-  printf("Por favor, digite 5 numeros: ");
+  printf("Por favor, digite 5 numeros:\n");
   for(i = 0; i < 5; i++){
     scanf("%d", &num);
     tmp = appendNode(tmp, num);
     if(first == NULL)
       first = tmp;
   }
+  printf("\n");
   return first;
 }
 
-LinkedNode *reverseList(LinkedNode *first){
-    LinkedNode *tmp = first, *prev, *next;
-
+LinkedNode *reverseList(LinkedNode *seq){
+    LinkedNode  *prev = NULL, *next;
+    while(seq->next != NULL){
+      next = seq->next;
+      seq->next = prev;
+      prev = seq;
+      seq = next;
+    }
+    printList(seq);
+    return seq;
 }
 
 int main(){
+  // primeira sequencia
   LinkedNode *seq1 = makeList();
-  printList(seq1);  // imprimiu primeira sequencia
-
-  LinkedNode *appendedNode = appendNode(seq1, 98); // coloquei um item no meio
   printList(seq1);
-
+  /* adicionando um elemento apos o primeiro elemento;
+  LinkedNode *appendedNode = appendNode(seq1, 98);
+  printList(seq1);
+  // segunda sequencia, concatena com a primeira
   LinkedNode *seq2 = makeList();
-  printList(seq2);  // imprimiu segunda sequencia
-  connectList(seq1, seq2);  // concatena a segunda na primeira
+  printList(seq2);
+  connectList(seq1, seq2);
   printf("Lista concatenada! ");
-  printList(seq1);  // imprime lista concatenada
-
+  printList(seq1);
+  // adiciona um elemento na posicao 4
+  addNode(seq1, 4);
+  printList(seq1);*/
+  // inverte a lista ligada
+  seq1 = reverseList(seq1);
+  printList(seq1);
 
   return 0;
 }
